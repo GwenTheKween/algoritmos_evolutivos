8016603db18738f9e672d3a2858a3885ceec5381
billionai <larsen.bruno.p@gmail.com>
2018-08-10 17:29:25 -0300

    Commited python test examples

---
+++ hard_coded/elitismo.py
@@ -0,0 +1,98 @@
+#primeira tentativa de algritmo genetico
+#usando o metodo do elitismo
+#apelidado de "o melhor transa com todos"
+
+import matplotlib.pyplot as plt
+import numpy as np
+from random import random
+
+def gera_individuo():
+    #gera um numero inteiro entre -50 e 50
+    return int(random()*100)
+
+def transa(pai,mae):
+    return (pai+mae)/2 + 2*random() -1
+
+def avalia(x):
+    return (-1)*x*(x-100)
+
+#--------------------------------------------------------------------------------
+#variaveis
+
+ind=[] #individuos
+notas=[]
+TamPop=4 #tamanho da populacao
+qtde_geracoes=20 #quantidade de iteracoes
+grafico_ind=[[] for i in range(TamPop)]
+grafico_notas=[[] for i in range(TamPop)]
+
+#inicia as populacoes
+for i in range(TamPop):
+    ind.append(gera_individuo())
+
+#iteracoes do algoritmo
+for j in range(qtde_geracoes):
+    #avalia a viabilidade
+    notas=[avalia(i) for i in ind]
+
+    #salva os dados para plotar os graficos
+    for i in range(TamPop):
+        grafico_ind[i].append(ind[i])
+        grafico_notas[i].append(notas[i])
+
+    #escolhe o individuo mais viavel
+    mx=0
+    for i in range(1,TamPop):
+        if(notas[i]>notas[mx]):
+            mx=i
+    #gera os novos individuos, matando todos os antigos
+    ind=[transa(ind[i],ind[mx]) for i in range(TamPop)]
+
+print(ind[mx],avalia(ind[mx]))
+
+#termina os dados do grafico
+for i in range(TamPop):
+    grafico_ind[i].append(ind[i])
+    #eh necessario re-avaliar pq a ultima iteracao termina gerando individuos
+    grafico_notas[i].append(avalia(ind[i]))
+
+
+#processamento para plotar os graficos
+t=np.arange(0,100)
+y=avalia(t)
+
+plt.subplot(321)
+plt.plot(t,y,'k')
+for i in range(qtde_geracoes):
+    plt.plot([grafico_ind[0][i],grafico_ind[0][i]],[grafico_ind[0][i],grafico_notas[0][i]],'b--')
+    plt.plot([grafico_ind[0][i],grafico_ind[0][i]],[grafico_notas[0][i],grafico_notas[0][i]],'b--')
+
+plt.subplot(322)
+plt.plot(t,y,'k')
+for i in range(qtde_geracoes):
+    plt.plot([grafico_ind[1][i],grafico_ind[1][i]],[grafico_ind[1][i],grafico_notas[1][i]],'b--')
+    plt.plot([grafico_ind[1][i],grafico_ind[1][i]],[grafico_notas[1][i],grafico_notas[1][i]],'b--')
+
+plt.subplot(323)
+plt.plot(t,y,'k')
+for i in range(qtde_geracoes):
+    plt.plot([grafico_ind[2][i],grafico_ind[2][i]],[grafico_ind[2][i],grafico_notas[2][i]],'b--')
+    plt.plot([grafico_ind[2][i],grafico_ind[2][i]],[grafico_notas[2][i],grafico_notas[2][i]],'b--')
+
+plt.subplot(324)
+plt.plot(t,y,'k')
+for i in range(qtde_geracoes):
+    plt.plot([grafico_ind[3][i],grafico_ind[3][i]],[grafico_ind[3][i],grafico_notas[3][i]],'b--')
+    plt.plot([grafico_ind[3][i],grafico_ind[3][i]],[grafico_notas[3][i],grafico_notas[3][i]],'b--')
+
+plt.subplot(313)
+t=[i for i in range(qtde_geracoes+1)]
+plt.plot(t,[50 for i in t],'k')
+plt.plot(t,grafico_ind[0],'b--')
+plt.plot(t,grafico_ind[1],'y--')
+plt.plot(t,grafico_ind[2],'r--')
+plt.plot(t,grafico_ind[3],'m--')
+
+plt.savefig('elitismo')
+
+plt.show()
---
+++ hard_coded/tretoso.py
@@ -0,0 +1,109 @@
+#primeira tentativa de algritmo genetico
+#usando o metodo do elitismo
+#apelidado de "o melhor transa com todos"
+
+import matplotlib.pyplot as plt
+import numpy as np
+from random import random
+
+def gera_individuo():
+    #gera um numero inteiro entre 0 e 200
+    return int(random()*200)
+
+def transa(pai,mae):
+    if(pai is mae): #evita auto transa
+	    return mae
+    filho=(pai+mae)/2
+    diff=np.abs(pai-mae)
+    return filho + 2*(random() - 0.5)
+
+def avalia(x):
+    return x*(x-200)*np.sin(x)
+
+def ajeita_plot(ind,notas):
+    y=[0,notas[0]]
+    for i in range(1,len(notas)):
+        y+=[notas[i-1],notas[i]]
+    x=[]
+    for i in ind:
+	    x+=([i]*2)
+    return x,y
+
+#--------------------------------------------------------------------------------
+#variaveis
+
+ind=[] #individuos
+notas=[]
+TamPop=8 #tamanho da populacao
+qtde_geracoes=200 #quantidade de iteracoes
+grafico_ind=[[] for i in range(TamPop)]
+grafico_notas=[[] for i in range(TamPop)]
+
+#inicia as populacoes
+for i in range(TamPop):
+    ind.append(gera_individuo())
+
+#iteracoes do algoritmo
+for j in range(qtde_geracoes):
+    #avalia a viabilidade
+    notas=[avalia(i) for i in ind]
+
+    #salva os dados para plotar os graficos
+    for i in range(TamPop):
+        grafico_ind[i].append(ind[i])
+        grafico_notas[i].append(notas[i])
+
+    #escolhe o individuo mais viavel
+    mx=0
+    for i in range(1,TamPop):
+        if(notas[i]>notas[mx]):
+            mx=i
+    #gera os novos individuos, matando todos os antigos
+    ind=[transa(ind[i],ind[mx]) for i in range(TamPop)]
+
+print(ind[mx],avalia(ind[mx]))
+
+#termina os dados do grafico
+for i in range(TamPop):
+    grafico_ind[i].append(ind[i])
+    #eh necessario re-avaliar pq a ultima iteracao termina gerando individuos
+    grafico_notas[i].append(avalia(ind[i]))
+
+
+#processamento para plotar os graficos
+t=np.linspace(0,200,1000)
+Y=avalia(t)
+
+
+plt.subplot(321)
+plt.plot(t,Y,'k')
+x,y=ajeita_plot(grafico_ind[0],grafico_notas[0])
+for i in range(qtde_geracoes):
+    plt.plot(x,y,'b--')
+
+plt.subplot(322)
+plt.plot(t,Y,'k')
+x,y=ajeita_plot(grafico_ind[1],grafico_notas[1])
+for i in range(qtde_geracoes):
+    plt.plot(x,y,'b--')
+
+plt.subplot(323)
+plt.plot(t,Y,'k')
+x,y=ajeita_plot(grafico_ind[2],grafico_notas[2])
+for i in range(qtde_geracoes):
+    plt.plot(x,y,'b--')
+
+plt.subplot(324)
+plt.plot(t,Y,'k')
+x,y=ajeita_plot(grafico_ind[3],grafico_notas[3])
+for i in range(qtde_geracoes):
+    plt.plot(x,y,'b--')
+
+plt.subplot(313)
+t=[i for i in range(qtde_geracoes+1)]
+for individuo in grafico_ind:
+    plt.plot(t,individuo,'--')
+
+plt.savefig('tretoso')
+
+plt.show()
