dd21bbffafe0d69c161da00d536c509ed45646be
Racdi <rpclerici@gmail.com>
2018-09-27 16:28:38 -0300

    Melhora nos debugs e possível correção

--- bibs/evo.h
+++ bibs/evo.h
@@ -133,7 +133,9 @@ template<class type> std::vector<type> evolutivo<type>::transa_por_roleta(){
 	double sum=0,negs=notas[0];
 	std::vector<type> nova_geracao;
 	for (int i = 0; i < notas.size(); i++) {
-		if (notas[i] > negs) negs = notas[i];
+		if (notas[i] < negs) {
+			negs = notas[i];
+		}
 	}
 	for (int i = 0; i < notas.size(); i++) {
 		sum += notas[i]-negs;
@@ -148,7 +150,7 @@ template<class type> std::vector<type> evolutivo<type>::transa_por_roleta(){
 		pai *= sum;
 		mae /= RAND_MAX;
 		mae *= sum;
-		std::cout << pai << '\t' << mae << std::endl;
+		std::cout << "Valor sorteado (p):" << pai << '\t' << "Valor sorteado (m):" << mae << std::endl;
 
 		//seleciona o pai
 		int i = -1;
@@ -156,16 +158,16 @@ template<class type> std::vector<type> evolutivo<type>::transa_por_roleta(){
 			i++;
 			pai -= notas[i];
 		}
-		std::cout << i << '\t';
+		std::cout << "Pai escolhido:"<< i << '\t';
 		//seleciona a mae
 		int j = -1;
 		while (mae > 0) {
 			j++;
 			mae -= notas[j];
 		}
-		std::cout << j << '\n';
+		std::cout << "Mae escolhida:"<< j << '\n';
 
-		nova_geracao.push_back(individuo[j-1].transa(individuo[i-1], range));
+		nova_geracao.push_back(individuo[j].transa(individuo[i], range));
 
 	}
 	return nova_geracao;
